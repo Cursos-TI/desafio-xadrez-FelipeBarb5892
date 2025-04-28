1e4be6ca20cde5f0f753fdddbc21e6f6df627c96
Felipe Barbosa <felipebcm79@gmail.com>
2025-04-28 14:48:20 +0000

    codigo nivel novato

--- xadrez.c
+++ xadrez.c
@@ -1,32 +1,165 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
+int main(){
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+    //criando as variaveis para usar no for e nos swithcs
+    int x , y, z;
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    //fazendo menu de boas vindas e pedindo ao usuario que escolha a peça que ele quer mover
+    printf("-- Bem vindo ao seu jogo de Xadrez --\n");
 
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    printf("1. Torre.\n");
+    printf("2. Rainha.\n");
+    printf("3. Bispo.\n");
+    printf("Escolha uma peça para mover 5 casas: ");
+    scanf("%d" , &x);
 
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    printf(" \n");
 
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    //fazendo o switch para cada peça que o usuario possa escolher
+    switch (x)
+    {
+    case 1:
+        //outro menu para o usuario escolher a direção que quer mover a peça e abaixo a logica do for para mostrar o resultado da ação escolhida pelo usuario
+        printf("1. Direita\n");
+        printf("2. Esquerda\n");
+        printf("3. Cima\n");
+        printf("4. Baixo\n");
+        printf("Escolha a direção que você ira mover a torre: ");
+        scanf("%d" , &z);
+        switch (z)
+        {
+        case 1:
+            for(y = 1; y <= 5; y++){
+            printf("Torre moveu uma casa para direita\n");
+        }
+        break;
+        case 2:
+            for(y = 1; y <= 5; y++){
+            printf("Torre moveu uma casa para esquerda\n");
+        }
+        break;
+        case 3:
+            for(y = 1; y <= 5; y++){
+            printf("Torre moveu uma casa para cima\n");
+        }
+        break;
+        case 4:
+            for(y = 1; y <= 5; y++){
+            printf("Torre moveu uma casa para baixo\n");
+        }
+        break;
+        default:
+        printf("Opção invalida.");
+            break;
+        }
+        
+        break;
+    case 2:
+        //outro menu para o usuario escolher a direção que quer mover a peça e abaixo a logica do for para mostrar o resultado da ação escolhida pelo usuario
+        printf("1. Direita\n");
+        printf("2. Esquerda\n");
+        printf("3. Cima\n");
+        printf("4. Baixo\n");
+        printf("5. Cima direita\n");
+        printf("6. Cima esquerda\n");
+        printf("7. Baixo esquerda\n");
+        printf("8. Baixo direita\n");
+        printf("Escolha a direção que você ira mover a rainha: ");
+        scanf("%d" , &z);
+        switch (z)
+        {
+        case 1:
+            for(y = 1; y <= 5; y++){
+            printf("Rainha moveu uma casa para direita\n");
+        }
+        break;
+        case 2:
+            for(y = 1; y <= 5; y++){
+            printf("Rainha moveu uma casa para esquerda\n");
+        }
+        break;
+        case 3:
+            for(y = 1; y <= 5; y++){
+            printf("Rainha moveu uma casa para cima\n");
+        }
+        break;
+        case 4:
+            for(y = 1; y <= 5; y++){
+            printf("Rainha moveu uma casa para baixo\n");
+        }
+        break;
+        case 5:
+            for(y = 1; y <= 5; y++){
+            printf("Rainha moveu uma casa para cima direita\n");
+        }
+        break;
+        case 6:
+            for(y = 1; y <= 5; y++){
+            printf("Rainha moveu uma casa para cima esquerda\n");
+        }
+        break;
+        case 7:
+            for(y = 1; y <= 5; y++){
+            printf("Rainha moveu uma casa para baixo esquerda\n");
+        }
+        break;
+        case 8:
+            for(y = 1; y <= 5; y++){
+            printf("Rainha moveu uma casa para baixo direita\n");
+        }
+        break;
+        default:
+        printf("Opção invalida.");
+            break;
+        }
+        break;
+    case 3:
+        //outro menu para o usuario escolher a direção que quer mover a peça e abaixo a logica do for para mostrar o resultado da ação escolhida pelo usuario
+        printf("1. Cima direita\n");
+        printf("2. Cima esquerda\n");
+        printf("3. Baixo esquerda\n");
+        printf("4. Baixo direita\n");
+        printf("Escolha a direção que você ira mover o bispo: ");
+        scanf("%d" , &z);
+        switch (z)
+        {
+        case 1:
+            for(y = 1; y <= 5; y++){
+            printf("Bispo moveu uma casa para cima direita\n");
+        }
+        break;
+        case 2:
+            for(y = 1; y <= 5; y++){
+            printf("Bispo moveu uma casa para cima esquerda\n");
+        }
+        break;
+        case 3:
+            for(y = 1; y <= 5; y++){
+            printf("Bispo moveu uma casa para baixo esquerda\n");
+        }
+        break;
+        case 4:
+            for(y = 1; y <= 5; y++){
+            printf("Bispo moveu uma casa para baixo direita\n");
+        }
+        break;
+        default:
+        printf("Opção invalida.");
+            break;
+        }
+        break;
+    
+    default:
+        printf("Opção invalida.\n");
+        break;
+    }
 
-    // Nível Mestre - Funções Recursivas e Loops Aninhados
-    // Sugestão: Substitua as movimentações das peças por funções recursivas.
-    // Exemplo: Crie uma função recursiva para o movimento do Bispo.
-
-    // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
-    // Inclua o uso de continue e break dentro dos loops.
+    printf(" \n");
+    //fim do jogo
+    printf("Obrigado por jogar ;)\n");
 
     return 0;
+
+
 }
